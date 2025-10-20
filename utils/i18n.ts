3e4e0159b324f0a66e11a3958a50865afe4ba14f
Ariedam64 <manitou88.jeux@gmail.com>
2025-10-20 14:53:03 +0200

    fix i18n.ts type import

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -1,7 +1,8 @@
 import i18n from 'i18next';
 import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import { initReactI18next, TFunction } from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
+import type { TFunction } from 'i18next';
 
 // Configuration centralisée
 const nextI18NextConfig = {
