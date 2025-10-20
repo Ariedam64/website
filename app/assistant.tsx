33aefbbc511c55a5dbc9b1024d3bb6ae5daf840e
Ariedam64 <manitou88.jeux@gmail.com>
2025-10-20 14:40:41 +0200

    fix assistant.tsx import

--- app/assistant.tsx
+++ app/assistant.tsx
@@ -2,8 +2,8 @@
 
 import { AssistantRuntimeProvider } from "@assistant-ui/react";
 import { useChatRuntime } from "@assistant-ui/react-ai-sdk";
-import { Thread } from "@/components/thread";
-import { ThreadList } from "@/components/thread-list";
+import { Thread } from "./components/assistant-ui/thread";
+//import { ThreadList } from "@/components/thread-list";
 
 export const Assistant = () => {
   const runtime = useChatRuntime({
@@ -13,7 +13,6 @@ export const Assistant = () => {
   return (
     <AssistantRuntimeProvider runtime={runtime}>
       <div className="grid h-dvh grid-cols-[200px_1fr] gap-x-2 px-4 py-4">
-        <ThreadList />
         <Thread />
       </div>
     </AssistantRuntimeProvider>
